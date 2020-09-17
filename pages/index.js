d82901cc9b19cc587646a96df81dd5683545186e
stalimatos <71282350+stalimatos@users.noreply.github.com>
2020-09-17 03:37:14 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,3 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
-export default function Home() {
-  return (
-      return <h1>Your new server-side rendered React.js app!</h1>
-
-  )
+export default () => {
+  return <h1>Your new server-side rendered React.js app!</h1>
 }
